7c9f072cbde208743d8425634419d160175b8625
WindnBike <windnbike@qq.com>
2021-02-08 09:07:48 +0800

    2021-02-08 09:07:48

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,17 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome package/AdGuardHome
+git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddns
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone --recursive -j8 https://github.com/jerrykuku/openwrt-package.git package/jerrykuku
+#git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
+sed -i 's/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/luci-app-smartdns/Makefile
+rm -r package/lean/luci-theme-argon
